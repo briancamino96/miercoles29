09fdd90c597496d49ef40655937ad92fd0c2849d
briancamino96 <bryan.camino.29@gmail.com>
2023-03-29 23:54:52 +0000

    modulos

---
+++ modulos/main.tf
@@ -0,0 +1,42 @@
+provider "azurerm" {
+    features {}
+}
+resource "azurerm_resource_group" "rsgr" {
+  name     = var.name_rg
+  location = var.locations_code
+}
+resource "azurerm_storage_account" "stac" {
+  name                     = var.name_stac
+  resource_group_name      = azurerm_resource_group.rsgr.name
+  location                 = azurerm_resource_group.rsgr.location
+  account_tier             = "Standard"
+  account_replication_type = var.name_replication
+}
+resource "azurerm_key_vault" "azkv" {
+  name                        = var.name_azkv
+  location                    = azurerm_resource_group.rsgr.location
+  resource_group_name         = azurerm_resource_group.rsgr.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  
+  sku_name = var.skuname_kvm1
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+  }
---
+++ modulos/var.tfvars
@@ -0,0 +1,9 @@
+#Datos Globales
+globals = {
+}
+locations_code = "eastus2"
+name_rg = "RSGRMIERCOLES1"
+name_stac = "miercoles1"
+name_replication = "LRS"
+name_azkv = "azkvmiercoles1"
+skuname_kvm1 = "premium"
